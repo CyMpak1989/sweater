a558e1331a02f69d2d3ab1945a88d306ef442517
CyMpak1989 <cympak2009@mail.ru>
2018-11-19 23:57:19 +0300

    Первый коммит остановился на Freemarker

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vladimir Lembikov (cympak2009@mail.ru) on 14.11.2018.
+ * @version 1.0.
+ * @since 0.1.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
